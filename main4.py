8a62aa32ac6e2ae1c431a77d969a2937ed25caba
McMullen <mabmcmul@iu.edu>
2019-08-28 20:05:15 -0400

    Made more changes

--- main4.py
+++ main4.py
@@ -17,11 +17,11 @@ for x in range(100,800,150):
         #start at 100, go to 599, counting by 150
         for y in range(100,600,150):
                 face_x,face_y = (x,y)
-                smile_x,smile_y = (face_x + 0,face_y - 0)
-                eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-                eye2_x,eye2_y = (face_x + 0,face_y + 0)
-                catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-                catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+                smile_x,smile_y = (face_x + 0,face_y - 10)
+                eye1_x,eye1_y = (face_x - 30,face_y + 45) 
+                eye2_x,eye2_y = (face_x + 30,face_y + 45)
+                catch1_x,catch1_y = (face_x - 25,face_y + 55) 
+                catch2_x,catch2_y = (face_x + 35,face_y + 55) 
 
 
                 # Draw the smiley face:
